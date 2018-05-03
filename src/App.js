d2913ca701fd0c2f9177d79f082959ef6bc6b688
Joe Bourne <josephbourne14@gmail.com>
2018-05-03 13:01:50 +0100

    Quick correct / wrong logic for dummy data

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import './App.css';
 
 import Question from './components/Question';
 
+// temp dummy data
+const images = [
+  { type: 'chalk', url: '/1' },
+  { type: 'cheese', url: '/2' },
+  { type: 'cheese', url: '/3' },
+  { type: 'chalk', url: '/4' },
+  { type: 'cheese', url: '/5' }
+];
+
 class App extends Component {
+  onClickChalk = index => {
+    images[index].type === 'chalk'
+      ? console.log('correct')
+      : console.log('wrong');
+  };
+
+  onClickCheese = index => {
+    images[index].type === 'cheese'
+      ? console.log('correct')
+      : console.log('wrong');
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Chalk or Cheese?</h1>
         </header>
-        <Question />
+        <Question
+          url={images[0].url}
+          onClickChalk={() => this.onClickChalk(0)}
+          onClickCheese={() => this.onClickCheese(0)}
+        />
       </div>
     );
   }
