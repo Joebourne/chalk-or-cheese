d8dcf17c200895d0379bb225f7c94b5abaaed3fa
Joe Bourne <josephbourne14@gmail.com>
2018-05-03 12:56:22 +0100

    Basic markup and props for Question

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Question() {
-  return <h2>Question 1</h2>;
+Question.propTypes = {
+  url: PropTypes.string.isRequired,
+  onClickChalk: PropTypes.func.isRequired,
+  onClickCheese: PropTypes.func.isRequired
+};
+
+export default function Question({ url, onClickChalk, onClickCheese }) {
+  return (
+    <div>
+      <div>
+        <img src={url} />
+      </div>
+      <hr />
+      <div>
+        <button onClick={onClickChalk}>chalk</button>
+        <button onClick={onClickCheese}>cheese</button>
+      </div>
+    </div>
+  );
 }
